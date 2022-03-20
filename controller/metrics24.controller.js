d15381a9e2d448be37ba934dbce8021a055938b4
Vladimir Kusov <vkusov@Vladimirs-MacBook-Air.local>
2022-03-20 19:45:54 +0300

    working filters

--- controller/metrics24.controller.js
+++ controller/metrics24.controller.js
@@ -1,9 +1,12 @@
 const db = require('../db')
 
 class Metrics24 {
-    async getData(req,res) {
-        const data = await db.query(`SELECT * FROM getdata('2022.1','IT4IT');`)
+    async getData(req,res,next) {
+        const strim = req.query.strim
+        const sprint = req.query.sprint
+        const data = await db.query(`select * from public.metric24_ where strim = \'${strim}\' and supersprint = \'${sprint}\'`)
         res.json(data.rows)
+        // res.json(`select * from public.metric24_ where strim = \'${strim}\' and supersprint = \'${sprint}\'`)
     }
 }
 
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 8080
+const app = express()
+
+app.use(cors())
 
 const router = require('./routes/metrics24.routes')
-const PORT = process.env.PORT || 8080
 
-const app = express()
+
+
 
 app.use(express.json())
 app.use('/api',router)
